e63a1559606ac9b4b0b0f70ddd541aaa6160ab42
marco2801 <marco20.rossi@mail.polimi.it>
2024-11-07 17:04:41 +0100

    simulation with slider for both radius and number of stetches

---
+++ simulation_sliders.py
@@ -0,0 +1,103 @@
+import time
+import pybullet as p
+import pybullet_data
+import numpy as np
+
+# Connect to PyBullet in GUI mode
+p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0, 0, -9.8)
+
+# Create the base plane for the simulation
+plane_id = p.loadURDF("plane.urdf")
+
+# Function to create a cylinder to represent the vessels
+def create_vessel(radius, length, position, orientation):
+    vessel_debug=[]
+    # Define the collision and visual shape
+    visual_shape_id = p.createVisualShape(shapeType=p.GEOM_CYLINDER, radius=radius, length=length,
+                                          rgbaColor=[0.8, 0.3, 0.3, 1.0], specularColor=[0.5, 0.5, 0.5])
+    collision_shape_id = p.createCollisionShape(shapeType=p.GEOM_CYLINDER, radius=radius, height=length)
+    # Create the cylinder as a rigid body
+    vessel_id = p.createMultiBody(baseMass=0, baseCollisionShapeIndex=collision_shape_id,
+                                  baseVisualShapeIndex=visual_shape_id, basePosition=position,
+                                  baseOrientation=orientation)
+    vessel_debug.append(vessel_id)
+    return vessel_debug
+
+# Function to generate points along the edges of the vessels
+def get_stiches(center, radius, num_stiches, offset=0.000):
+    debug_items = []
+    for i in range(num_stiches):
+        angle = 2 * np.pi * i / num_stiches
+        x_position = center[0]
+        y_position = center[1] + radius * np.sin(angle) + offset  # Add an offset to see the points
+        z_position = center[2]+ radius * np.cos(angle) + offset
+        point_id = p.addUserDebugPoints([[x_position, y_position, z_position]],
+                                        pointColorsRGB=[[0, 1, 0]], pointSize=5)
+        debug_items.append(point_id)
+    return debug_items
+
+# Vessel parameters
+vessel_length = 0.3       # Length of the vessels (30 cm)
+gap = 0.02                # Space of 2 mm between vessel ends
+
+
+#slider for the number of sutere points
+suture_slider = p.addUserDebugParameter("Num Suture Points", 3, 20, 7)
+#slider for vessel radius
+radius_slider=p.addUserDebugParameter("Vessel Radius",0.03,0.15,0.05)
+
+# variables for traking ids
+debug_points_ids = []  # point ids list
+previous_suture_points = 0  # check if the number has changed
+
+# Configure the camera for a better view
+camera_angle = 0.05
+p.resetDebugVisualizerCamera(cameraDistance=1.0, cameraYaw=45, cameraPitch=-30,
+                             cameraTargetPosition=[0, 0, camera_angle])
+debug_vessel_ids=[]
+previous_vessel_radius=0
+# Simulation
+while True:
+    p.stepSimulation()
+    # read the value in the slider
+    num_suture_points = int(round(p.readUserDebugParameter(suture_slider)))
+    vessel_radius = p.readUserDebugParameter(radius_slider)
+    # Position and orientation of the vessels to represent the anastomosis
+    # Position the vessels along the X-axis with 2 mm of space between the ends
+    position_vessel1 = [-vessel_length / 2 - gap / 2, 0, vessel_radius]
+    position_vessel2 = [vessel_length / 2 + gap / 2, 0, vessel_radius]
+    orientation_vessel = p.getQuaternionFromEuler([0, np.pi / 2, 0])  # Horizontal orientation
+
+    # Create the two vessels
+
+    center_vessel1 = [position_vessel1[0] + vessel_length / 2, position_vessel1[1], position_vessel1[2]]
+    center_vessel2 = [position_vessel2[0] - vessel_length / 2, position_vessel2[1], position_vessel2[2]]
+
+    # delete the old vessels if the radius change and build the new ones
+    if vessel_radius != previous_vessel_radius:
+        for vessel_id in debug_vessel_ids:
+            p.removeBody(vessel_id)
+        # update the new vessels
+        debug_vessel_ids = create_vessel(vessel_radius, vessel_length, position_vessel1, orientation_vessel)
+        debug_vessel_ids += create_vessel(vessel_radius, vessel_length, position_vessel2, orientation_vessel)
+        previous_vessel_radius = vessel_radius
+
+
+
+
+    # if the number of steatches change remove the old points
+    if num_suture_points != previous_suture_points:
+        # removal
+        for point_id in debug_points_ids:
+            p.removeUserDebugItem(point_id)
+
+        # update the new points
+        debug_points_ids = get_stiches(center_vessel1, vessel_radius, num_suture_points)
+        debug_points_ids += get_stiches(center_vessel2, vessel_radius, num_suture_points)
+
+
+        previous_suture_points = num_suture_points
+
+    time.sleep(0.1)
